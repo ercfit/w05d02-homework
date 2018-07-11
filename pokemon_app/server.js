d13fbf3768beb6c53bb2ef7459b198d16c6fa670
Erica Bruenton <ercfit@gmail.com>
2018-07-11 09:46:35 -0600

    Commit 6 - I can view a list of all my pokemon in the browser

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -4,32 +4,28 @@
 	//in pokemon_app
 //What is the name of the file?
 	//package.json
-   
-//"Commit 1 - All my files are created"
-
-//"Commit 2 - All my npm packages are added"
 
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const Pokemon = require("./models/pokemon")
+const Pokemon = require("./models/pokemon");
+
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
 	res.send("Welcome to the Pokemon App!");
 })
 
-//"Commit 3 - My server is set up and running"
-
 app.get("/pokemon", (req, res) => {
 	res.render("index.ejs", {"pokemonList":Pokemon})
 });
 
-//"Commit 4 - Connected my database, can see json in the browser"
-//"Commit 5 - index.ejs view rendered at pokemon route"
+//     "Commit 6 - I can view a list of all my pokemon in the browser "
+// <hr>
 
 
 
-
+		
 app.listen(PORT, () => {
 	console.log("I am listening on port 3000.");
 })
